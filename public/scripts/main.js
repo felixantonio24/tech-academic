f1811e3ef86bfcafa3cdbbd62527bcd39b7c44ea
felix <luisfelixtv@outlook.es>
2025-03-10 00:11:14 -0500

    Primer commit: Proyecto de Astro

---
+++ public/scripts/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("btn-inscripcion");
+  
+    button.addEventListener("click", () => {
+      document.getElementById("inscripcion").scrollIntoView({ behavior: "smooth" });
+    });
+  });
+  const abrir = document.querySelector(".menu-button");
+
